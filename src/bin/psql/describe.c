8ef7a86f626d441c781fd83828cbbdb907afaf02
Adam Brightwell <adam.brightwell@gmail.com>
2014-07-28 18:58:52 -0400

    RLS - update psql for row security.

--- src/bin/psql/describe.c
+++ src/bin/psql/describe.c
@@ -2796,6 +2796,11 @@ listTables(const char *tabtypes, const char *pattern, bool verbose, bool showSys
 		appendPQExpBuffer(&buf,
 			  ",\n  pg_catalog.obj_description(c.oid, 'pg_class') as \"%s\"",
 						  gettext_noop("Description"));
+
+		if (pset.sversion >= 90500)
+			appendPQExpBuffer(&buf,
+				",\n pg_catalog.pg_get_expr(rs.rsecqual, c.oid) as \"%s\"",
+				gettext_noop("Row-security"));
 	}
 
 	appendPQExpBufferStr(&buf,
@@ -2805,6 +2810,9 @@ listTables(const char *tabtypes, const char *pattern, bool verbose, bool showSys
 		appendPQExpBufferStr(&buf,
 			 "\n     LEFT JOIN pg_catalog.pg_index i ON i.indexrelid = c.oid"
 		   "\n     LEFT JOIN pg_catalog.pg_class c2 ON i.indrelid = c2.oid");
+	if (verbose && pset.sversion >= 90500)
+		appendPQExpBuffer(&buf,
+			 "\n     LEFT JOIN pg_rowsecurity rs ON rs.rsecrelid = c.oid");
 
 	appendPQExpBufferStr(&buf, "\nWHERE c.relkind IN (");
 	if (showTables)
