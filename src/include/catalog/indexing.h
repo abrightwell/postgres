be56ce3ab5b66fd21bdebc58f6c2fb3f69eb7dca
Adam Brightwell <adam.brightwell@gmail.com>
2014-07-29 18:20:58 -0400

    SU - add pg_permission system catalog.

--- src/include/catalog/indexing.h
+++ src/include/catalog/indexing.h
@@ -299,6 +299,12 @@ DECLARE_UNIQUE_INDEX(pg_extension_name_index, 3081, on pg_extension using btree(
 DECLARE_UNIQUE_INDEX(pg_range_rngtypid_index, 3542, on pg_range using btree(rngtypid oid_ops));
 #define RangeTypidIndexId					3542
 
+DECLARE_UNIQUE_INDEX(pg_permission_oid_index, 6001, on pg_permission using btree(oid oid_ops));
+#define PermissionOidIndexId				6001
+
+DECLARE_UNIQUE_INDEX(pg_permission_roleid_index, 6002, on pg_permission using btree(permroleid oid_ops));
+#define PermissionRoleIdIndexId				6002
+
 /* last step of initialization script: build the indexes declared above */
 BUILD_INDICES
 
---
+++ src/include/catalog/pg_permission.h
@@ -0,0 +1,47 @@
+/*
+ * pg_permission.h
+ *   definition of the system catalog for role permissions (pg_permission)
+ *
+ * Portions Copyright (c) 1996-2012, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ */
+#ifndef PG_PERMISSION_H
+#define PG_PERMISSION_H
+
+#include "catalog/genbki.h"
+
+/* ----------------
+ *		pg_permission definition.  cpp turns this into
+ *		typedef struct FormData_pg_permission
+ * ----------------
+ */
+#define PermissionRelationId	6000
+
+CATALOG(pg_permission,6000)
+{
+	Oid		permroleid;
+	int32	permpermission;
+} FormData_pg_permission;
+
+/* ----------------
+ *		Form_pg_permission corresponds to a pointer to a tuple with
+ *		the format of pg_permission relation.
+ * ----------------
+ */
+typedef FormData_pg_permission *Form_pg_permission;
+
+/*
+ * ----------------
+ * 		compiler constants for pg_permission
+ * ----------------
+ */
+#define Natts_pg_permission						2
+#define Anum_pg_permission_permroleid			1
+#define Anum_pg_permission_permpermission		1
+
+#define PERMISSION_CREATE_DB	0
+#define PERMISSION_CREATE_ROLE	1
+
+#endif   /* PG_PERMISSION_H */
+
